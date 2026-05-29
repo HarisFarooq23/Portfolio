02cc60cb67f31f3e1beb0e309166bf9a5ef76714
Haris Farooq <harisnetbackup@gmail.com>
2026-05-29 20:13:32 +0500

    updated project

--- artifacts/portfolio/src/App.tsx
+++ artifacts/portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import { CustomCursor } from "@/components/CustomCursor";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <CustomCursor />
       <Home />
       <Toaster />
+      <Analytics />
     </TooltipProvider>
   );
 }
